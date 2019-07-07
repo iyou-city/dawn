bc7f8f685d3d52c8177f936e50a3f068d5189bca
Daniel <jmzwcn@gmail.com>
2019-07-07 09:26:41 +0800

    Simple codes.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- internal/cmd/file_server.go
+++ internal/cmd/file_server.go
@@ -9,11 +9,11 @@ import (
 )
 
 const (
-	fileDir = "/uploads"
+	baseDir = "/uploads"
 )
 
 func serveFileUpload() {
-	fs := http.FileServer(http.Dir(fileDir))
+	fs := http.FileServer(http.Dir(baseDir))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/upload", fileUploadHandler)
 	http.ListenAndServe(":9090", nil)
@@ -22,6 +22,10 @@ func serveFileUpload() {
 func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	//设置内存大小
 	r.ParseMultipartForm(32 << 20)
+	//创建上传目录
+	r.ParseForm()
+	dir := baseDir + "/" + r.FormValue("title")
+	os.Mkdir(dir, os.ModePerm)
 	//获取上传的文件组
 	files := r.MultipartForm.File["uploadfile"]
 	len := len(files)
@@ -32,21 +36,16 @@ func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		//创建上传目录
-		r.ParseForm()
-		dir := fileDir +"/"+ r.FormValue("title")
-		os.Mkdir(dir, os.ModePerm)
-		//os.Mkdir("./upload", os.ModePerm)
+
 		//创建上传文件
 		cur, err := os.Create(dir + "/" + files[i].Filename)
-		//cur, err := os.Create("./upload/" + files[i].Filename)
 		defer cur.Close()
 		if err != nil {
 			log.Fatal(err)
 		}
 		io.Copy(cur, file)
-		fmt.Println(files[i].Filename)
-		//输出上传的文件名 }
-		fmt.Fprintf(w, "success!\n") //这个写入到w的是输出到客户端的
+		fmt.Println(files[i].Filename, "uploaded")
 	}
+	//输出上传的文件名
+	fmt.Fprintf(w, "success!\n") //这个写入到w的是输出到客户端的
 }
